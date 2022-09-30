d8ffd89b9909dd4da442e2cc6978df9d80793320
Albina <albina.exe@mail.ru>
2022-09-30 20:05:44 +0200

    Initial Commit

---
+++ Lesson 2.06/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 2.06
+//
+//  Created by Альбина Лега on 30/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
