adfe0925219f06bd2ee3ffae90864e68a46ae202
Albina <albina.exe@mail.ru>
2022-10-01 18:07:59 +0200

    Commented

--- Lesson 2.06/MainViewController.swift
+++ Lesson 2.06/MainViewController.swift
@@ -19,7 +19,8 @@ class MainViewController: UIViewController {
         
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) { // вызывается всегда при переходе по сигвею. В этом методе мы создаем экземпляр класса, на который собираемся перейти, для того, чтобы передать туда данные (на VC на который собираемся перейти)
+        //Это делается только в том случае, если нам нужно передать данные. Если передавать данные не нужно, то реализовывать методы тоже не нужно
         guard let settingsVC = segue.destination as? SettingsViewController else { return }
         settingsVC.minimumValue = minimumValueLabel.text
         settingsVC.maximumValue = maximumValueLabel.text
@@ -33,7 +34,7 @@ class MainViewController: UIViewController {
         randomValueLabel.text = Int.random(in: minimumNumber...maximumNumber).formatted()
     }
     
-    @IBAction func unwind(for segue: UIStoryboardSegue) {
+    @IBAction func unwind(for segue: UIStoryboardSegue) { // с этим методом мы связали кнопку SAVE. Этот метод используется только в том случае, когда нужно передать данные из того VC, с которого вы ушли, на тот, на текущий VC. Если данные передавать не нужно, то реализовывать метод тоже не нужно
         guard let settingsVC = segue.source as? SettingsViewController else { return }
         minimumValueLabel.text = settingsVC.minimumValueTF.text
         maximumValueLabel.text = settingsVC.maximumValueTF.text
